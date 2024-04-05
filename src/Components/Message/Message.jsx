548df1ade19760c1a1f64fcc6780b1e3e6988360
Aparna Pradeep <aparnapradeep31@gmail.com>
2024-04-05 14:39:04 +0530

    workinmg in  message box ui

--- src/Components/Message/Message.jsx
+++ src/Components/Message/Message.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Message.css';
+import UserMessage from "../UserMessage/UserMessage"
 import {  FaPaperPlane } from 'react-icons/fa';
 function Message() {
   return (
     <div className="message-area">
+      <div className="message-header">
+          In-call messages
+         
+      </div>
+      <div className="text-messages">
+        <UserMessage username='User-1' message='Hellooo'/>
+        <h4 className='user-name'>User-1</h4> <p className='user-message'>Heljjjgjgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggloooooooooooooooooooo</p>
+        <h4>User-1</h4> <p>Hello</p>
+        <h4>User-1</h4> <p>Hello</p>
+        <h4>User-1</h4> <p>Hello</p>
+        <h4>User-1</h4> <p>Hello</p>
+        <h4>User-1</h4> <p>Hello</p>
+        <h4>User1</h4> <p>Hello</p>
+        <h4>User1</h4> <p>Hello</p>
+        <h4>User1</h4> <p >Hello</p>
+      </div>
+
       <div className="text-field">
         <input type="text" className="text-input" />
         <FaPaperPlane className='send-icon' onClick={console.log('hi.................')}></FaPaperPlane>
---
+++ src/Components/UserMessage/UserMessage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UserMessage(props) {
+  
+return (
+    <div className="my-message">
+      {/* <h4>{props.username}</h4> */}
+    </div>
+    
+
+      
+      
+  
+  );
+}
+
+export default UserMessage
