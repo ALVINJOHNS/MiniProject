051064fca293a01464f0f319b10aba628d205e05
ALVINJOHNS <alvinjohnson2016@gmail.com>
2024-05-07 22:20:42 +0530

    css files updated for build error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import JoinPage from './Pages/JoinPage/JoinPage';
-// import JoinPage from './Pages/JoinPage/JoinPage';
 import VideoRoom from './Pages/VideoRoom/VideoRoom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
