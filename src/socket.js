518b8ee5642332deb6e472964cfdd62211c87ae7
ALVINJOHNS <alvinjohnson2016@gmail.com>
2024-05-08 19:55:46 +0530

    changed server TO LOCALHOST

--- src/socket.js
+++ src/socket.js
@@ -9,5 +9,6 @@ const socket = io.connect('https://localhost:8181/', {
 
 export default socket;
 
-export const socketForML = new WebSocket('ws://20.235.142.139/ws/hand_gesture/');
+export const socketForML = new WebSocket('ws://localhost:8000/ws/hand_gesture/');
+//export const socketForML = new WebSocket('ws://localhost:8000/ws/words/');
 //export const socketForML = new WebSocket('ws://20.235.142.139/ws/words/');
